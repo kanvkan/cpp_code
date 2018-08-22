3b0c5386209a19766e310a78a8fb9c83399fad0f
kanvkan <kanvkan@yeah.net>
2018-08-22 21:05:48 +0800

    update

--- tool/pthread_test/pthread_test.cc
+++ tool/pthread_test/pthread_test.cc
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<thread>
 #include<mutex>
+#include<unistd.h>
 #include"../mutex_lock/mutex_lock.h"
 
 std::mutex mtx;
@@ -13,12 +14,22 @@ void count_f(){
         std::cout<<t_id<<": "<<count<<std::endl;
     }
 }
+
+void f(){
+    int c = 100;
+    while(c >  0){
+	std::cout<<c<<std::endl;
+	c--;
+    }
+}
 int main(){
 
 
-    std::thread t1(count_f);
-    std::thread t2(count_f);
-
-    t1.join();
-    t2.join();
+    std::thread t1(f);
+    std::thread t2(f);
+    std::cout<<"main end"<<std::endl;
+    //t1.detach();
+    //t2.detach();
+    ////t1.join();
+//    t2.join();
 }
